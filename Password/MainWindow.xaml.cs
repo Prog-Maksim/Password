127cfae66570b0622b8faad904edbcbe4246d10e
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-07-07 23:26:18 +0300

    обновление окна

--- Password/MainWindow.xaml.cs
+++ Password/MainWindow.xaml.cs
@@ -10,8 +10,41 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
+    # region systemButton
     private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
     {
         DragMove();
     }
+
+    private void CloseWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Close();
+    }
+    
+    private bool _isMaximized;
+
+    private void WindowsMaksimum()
+    {
+        WindowState = WindowState.Maximized;
+        _isMaximized = true;
+    }
+
+    private void WindowsBase()
+    {
+        WindowState = WindowState.Normal;
+        _isMaximized = false;
+    }
+
+    private void WindowSize_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (_isMaximized) WindowsBase();
+        else WindowsMaksimum();
+    }
+
+    private void WindowMinimize_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+    
+    #endregion
 }
