c804756fdc5989f8980a17748adaa51182371914
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-07-07 23:03:41 +0300

    создание программы

---
+++ Password/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Password;
+
+
+public partial class App : Application
+{
+    
+}
---
+++ Password/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Password;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+}
