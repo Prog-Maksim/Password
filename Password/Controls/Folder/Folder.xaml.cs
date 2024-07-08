a596cb6046b9318d9b79fbfdccd8cae1b7e98922
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-07-08 13:07:06 +0300

    добавление папки

---
+++ Password/Controls/Folder/Folder.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Password.Controls.Folder;
+
+public partial class Folder : UserControl
+{
+    public Folder()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+    }
+}
