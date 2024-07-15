2ac7b1965131b9f72b9f542f9f077e26bd58fcce
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-07-15 14:03:28 +0300

    создание всплывающего меню для expander

---
+++ Password/Controls/Expander/Popup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Password.Controls.Expander;
+
+public partial class Popup
+{
+    private Expander.AddFolder? func;
+
+    public Popup()
+    {
+        InitializeComponent();
+
+        func = null;
+    }
+
+    public Popup(Expander.AddFolder func): this()
+    {
+        this.func = func;
+    }
+    
+    private void StoragePopupBorder_OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        ClosePopup();
+    }
+
+    public void ClosePopup()
+    {
+        StoragePopup.IsOpen = false;
+    }
+
+    public void IsOpenPopup()
+    {
+        StoragePopup.IsOpen = true;
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (func is not null)
+            func();
+    }
+
+    ~Popup()
+    {
+        StoragePopup = null;
+    }
+}
